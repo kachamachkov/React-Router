ac9fb5546585a77d821e8006440f2e0a0d19ccf0
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-14 17:31:50 +0300

    Add newer way of Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
 import About from './pages/About';
 
+// layout
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<RootLayout />}>
+			{/* <Route path='/' element={<Home />} /> */}
+			<Route index element={<Home />} />
+			<Route path='/about' element={<About />} />
+
+		</Route>
+	)
+);
+
 function App() {
 
 	return (
-		<BrowserRouter>
-			<header>
-				<nav>
-					<h1>React router</h1>
-					<NavLink to='/'>Home</NavLink>
-					<NavLink to='about'>About</NavLink>
-				</nav>
-			</header>
-			<main>
-				<Routes>
-					{/* <Route path='/' element={<Home />} /> */}
-					<Route index element={<Home />} />
-					<Route path='/about' element={<About />} />
+		<>
 
-				</Routes>
-			</main>
-		</BrowserRouter>
+			<RouterProvider router={router} />
+		</>
 	);
 };
 
