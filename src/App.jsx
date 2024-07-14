ecce332fe619fe5deb6a041e4af9b95c0ef18194
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-14 19:40:30 +0300

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-
-// pages
-import Home from './pages/Home';
-import About from './pages/About';
-
-// layout
-import RootLayout from './layouts/RootLayout';
-
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/' element={<RootLayout />}>
-			{/* <Route path='/' element={<Home />} /> */}
-			<Route index element={<Home />} />
-			<Route path='/about' element={<About />} />
-
-		</Route>
-	)
-);
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
 
-	return (
-		<>
-
-			<RouterProvider router={router} />
-		</>
-	);
-};
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes >
+  );
+}
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,3 @@
-const Home = () => {
-  return (
-    <div className="home">
-      <h2>Welcome</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nulla molestias sequi itaque. Odio enim consequatur sapiente nostrum dolor quae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nulla molestias sequi itaque. Odio enim consequatur sapiente nostrum dolor quae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nulla molestias sequi itaque. Odio enim consequatur sapiente nostrum dolor quae?</p>
-    </div>
-  );
-};
-export default Home;
+export function Home() {
+  return <h1>Home</h1>;
+}
