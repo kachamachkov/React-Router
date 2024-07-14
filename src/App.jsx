0bccb80097ea230b6b79baea6d2e52d2a3d6c19f
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-14 16:51:17 +0300

    Add header styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 			<header>
 				<nav>
 					<h1>React router</h1>
-					<Link to='/'>Home</Link>
+					<NavLink to='/'>Home</NavLink>
 					<NavLink to='about'>About</NavLink>
 				</nav>
 			</header>
